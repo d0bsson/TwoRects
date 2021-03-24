a292e126aa01570bb1ba2c0350a5d84ea1c15406
David <ber.dev@yandex.ru>
2021-03-25 00:27:11 +0300

    Initial Commit

---
+++ TwoRects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoRects
+//
+//  Created by Дэвид Бердников on 25.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
